78ef6c901285e24870838b10bdd2946a20fdaf8b
DariaCherr <DaskhenCher@mail.ru>
2023-06-30 20:33:37 +0300

    version1.2

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+const menu = document.querySelector("#menu").cloneNode(1);
+const burger = document.querySelector(".menu-burger");
+const popup = document.querySelector(".popup");
+const body = document.body;
+
+
+burger.addEventListener("click", changeMenu);
+
+function changeMenu(e) {
+    e.preventDefault();
+    popup.classList.toggle("open");
+    burger.classList.toggle("active");
+
+    renderPopup();
+}
+
+function renderPopup() {
+    popup.appendChild(menu);
+}
+
+const bgMenu = Array.from(menu.children);
+
+bgMenu.forEach((item) => {
+    item.style.display = 'block';
+});
+
+
+
+function closeOnClick() {
+    popup.classList.remove("open");
+    burger.classList.remove("active");
+
+}
+
+
+console.log();
