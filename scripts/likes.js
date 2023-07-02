3e6db8f076a54445095ad4bf8ac056aaf6a1e9eb
DariaCherr <DaskhenCher@mail.ru>
2023-07-02 16:32:15 +0300

    version1.6

---
+++ scripts/likes.js
@@ -0,0 +1,15 @@
+let hearts = document.querySelectorAll('.heart');
+let likesNumber = document.querySelectorAll('.likes-number');
+
+for (let item of hearts) {
+    item.addEventListener('click', function() {
+        if (item.classList.contains('added')) {
+            item.style.backgroundImage = "url('/icons/heart-regular.svg')";
+            likesNumber.textContent--;
+        } else {
+            item.style.backgroundImage = "url('/icons/heart-solid.svg')";
+            likesNumber.textContent++;
+        }
+        item.classList.toggle('added');
+    })
+}
